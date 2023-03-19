b2be0b49dac4cc0072a121e495ca079c625f3f44
Minji-torii <whalswl0110@gmail.com>
2023-03-19 13:32:18 +0900

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import styled from "styled-components";
+// Pages
+import MainPage from './page/MainPage';
+import PostWritePage from './page/PostWritePage';
+import PostViewPage from './page/PostViewPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+function App(props) {
+    return (
+        <BrowserRouter>
+            <MainTitleText>
+              KaKao Academy of Gachon Univ.
+            </MainTitleText>
+            <Routes>
+                <Route index element={<MainPage />} />
+                <Route path="post-write" element={<PostWritePage />} />
+                <Route path="post/:postId" element={<PostViewPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/miniblog_sytled.js
@@ -0,0 +1,180 @@
+//// Button.jsx
+const StyledButton = styled.button`
+    padding: 8px 16px;
+    font-size: 16px;
+    border-width: 1px;
+    border-radius: 8px;
+    cursor: pointer;
+`;
+
+////TextInput.jsx
+const StyledTextarea = styled.textarea`
+    width: calc(100% - 32px);
+    ${(props) => props.height &&  `height: ${props.height}px;`  }      padding: 16px;
+    font-size: 16px;
+    line-height: 20px;
+`;
+
+//////CommentListItem.jsx
+
+const Wrapper = styled.div`
+    width: calc(100% - 32px);
+    padding: 8px 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border: 1px solid grey;
+    border-radius: 8px;
+    cursor: pointer;
+    background: white;
+    :hover {
+        background: lightgrey;
+    }
+`;
+
+
+
+const ContentText = styled.p`
+    font-size: 16px;
+    white-space: pre-wrap;
+`;
+
+//////CommentList.jsx
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+//////////PostList.jsxconst Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+//////////PostListItem.jsx
+
+const Wrapper = styled.div`
+    width: calc(100% - 32px);
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border: 1px solid grey;
+    border-radius: 8px;
+    cursor: pointer;
+    background: white;
+    :hover {
+        background: lightgrey;
+    }
+`;
+const TitleText = styled.p`
+    font-size: 20px;
+    font-weight: 500;
+`;
+
+//////////MainPage.jsx
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+//////////////PostViewPage.jsx
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+const PostContainer = styled.div`
+    padding: 8px 16px;
+    border: 1px solid grey;
+    border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+    font-size: 28px;
+    font-weight: 500;
+`;
+
+const ContentText = styled.p`
+    font-size: 20px;
+    line-height: 32px;
+    white-space: pre-wrap;
+`;
+
+const CommentLabel = styled.p`
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+/////////////////PostWritePage.jsx
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
